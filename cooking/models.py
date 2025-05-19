9d92a5b80fd01102b3029a4d0e8ab8a70fc5f576
memessidy <mailboxforstudying@gmail.com>
2025-05-19 23:22:53 +0300

    related name added for category

--- cooking/models.py
+++ cooking/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
     photo = models.ImageField(upload_to='photos/', blank=True, null=True, verbose_name='Изображение')
     watched = models.IntegerField(default=0, verbose_name='Просмотры')
     is_published = models.BooleanField(default=False, verbose_name='Публикация')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', related_name='posts')
 
     def __str__(self):
         return self.title
